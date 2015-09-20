3b8c95a4e9ef33a0a7896370398d9ccccfdfb6a0
Manuel Meurer <manuel@krautcomputing.com>
2015-09-20 08:58:58 +0200

    fix error with recaptcha public key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY'],
+  config.public_key  = ENV['RECAPTCHA_PUBLIC_KEY']
   config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'https://ide.c9.io/attyc/appcity'
